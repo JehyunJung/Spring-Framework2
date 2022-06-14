3f71b26d003ec5ccc52e117fb29132ac55f76388
Jehyun Jung <toojey7@gmail.com>
2022-06-14 12:11:30 +0900

    feat: tested multiple bean DI
    use Map, List to get multiple DI's

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -9,12 +9,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core",
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION,classes=Configuration.class)
 )
 public class AutoAppConfig {
-    @Bean(name="memoryMemberRepository")
+    /*@Bean(name="memoryMemberRepository")
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
-    }
+    }*/
 }
